15ba96a0e41bf9a67796526eccf3611e9d11178f
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2025-02-01 20:57:09 +0530

    size change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ const page = () => {
     <>
     <div className='flex justify-between items-center p-4 bg-[#B7EFF8B2] rounded-tl-3xl min-h-screen rounded-tr-3xl mt-3'>
     <div className="flex flex-col space-y-14 ">
-     <p className='font-medium text-lg'>Your One - Step Plateform for</p>
-         <h1 className='space-y-10 font-bold text-6xl'>ALL YOUR NEEDS</h1>
-         <p className='font-medium text-lg'>Explore our tailored services and find solutions</p>
+     <p className='font-medium text-base'>Your One - Step Plateform for</p>
+         <h1 className='space-y-10 font-bold text-5xl'>ALL YOUR NEEDS</h1>
+         <p className='font-medium text-base'>Explore our tailored services and find solutions</p>
    
-         <p className='font-medium text-lg'>Unlock the power of AI, digital tools, and seamless solutions. Start simplifying your digital world today</p>
+         <p className='font-medium text-base'>Unlock the power of AI, digital tools, and seamless solutions. Start simplifying your digital world today</p>
    
          <button className='border border-black p-4  w-44 rounded-full '>EXLPORE NOW</button>
     </div>
